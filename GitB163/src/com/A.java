cdd4a4a17c67fb4dd215911f674811a0068a3a8c
mhaisanerahul <rahulmhaisane5@gmail.com>
2024-05-15 12:29:57 +0530

    Update A.java

--- GitB163/src/com/A.java
+++ GitB163/src/com/A.java
@@ -8,6 +8,9 @@ public class A {
 	public void m2() {
 		System.out.println("M1");
 	}
+	public void m3() {
+		System.out.println("M1");
+	}
 public static void main(String[] args) {
 	System.out.println("this is Git Demo");
 }
