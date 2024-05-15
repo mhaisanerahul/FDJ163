0834a40fcdd29aba2e860ad9c70d096b903ba01b
mhaisanerahul <rahulmhaisane5@gmail.com>
2024-05-15 11:52:47 +0530

    Git Rmh

--- GitB163/src/com/A.java
+++ GitB163/src/com/A.java
@@ -5,6 +5,9 @@ public class A {
 	public void m1() {
 		System.out.println("M1");
 	}
+	public void m2() {
+		System.out.println("M1");
+	}
 public static void main(String[] args) {
 	System.out.println("this is Git Demo");
 }
